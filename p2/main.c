afecc25c1a859b8d444427d08d8691696a2035f6
2025138077 <ddolieee@kau.kr>
2026-05-27 16:34:22 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -3,22 +3,18 @@
 int main(void) {
     int N;
     int arr1[20], arr2[20];
-    int *ptr1 = arr1, *ptr2 = arr2;
-    
+
     scanf("%d", &N);
-    for(int i = 0; i < N; i++) {
-        scanf("%d", (ptr1 + i));
+    for (int *p = arr1; p < arr1 + N; p++) {
+        scanf("%d", p);
     }
-    
-    for(int i = 0; i < N; i++) {
-        scanf("%d", (ptr2 + i));
+
+    for (int *q = arr2; q < arr2 + N; q++) {
+        scanf("%d", q);
     }
-    
-    for(int i = 0; i < N; i++) {
-        int a = *(ptr1 + i); 
-        int b = *(ptr2 + (N - 1 - i)); 
-        
-        printf(" %d", a + b);
+
+    for (int *p = arr1, *q = arr2 + N - 1; p < arr1 + N; p++, q--) {
+        printf(" %d", *p + *q);
     }
     
     return 0;
